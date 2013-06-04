a9c1e830639f7cb625868d3723ad24a920cb75a5
Miguel Palhas <mpalhas@gmail.com>
2013-06-04 10:45:18 +0100

    adds tests/gpu dir

---
+++ tests/ppm-rr/gpu/job.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+HOME      = ENV['HOME']
+NODE      = `hostname`.split.first
+KBEST     = "#{HOME}/projects/beast/blotter/kbest.rb"
+TEST_ROOT = "#{HOME}/projects/msc-thesis/tests/ppm-rr/gpu"
+
+THREADS = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+SCENES = ['kitchen', 'cornell', 'alloy', 'bigmonkey', 'simple', 'simple-mat', 'luxball']
+
+EXEC = "#{{}}"
+
+THREADS.each do |t|
+  SCENES.each do |scene|
+    puts "  THREADS = #{t}, SCENE = #{scene}"
+    this_test_root = "#{TEST_ROOT}/#{NODE}/#{scene}_t#{'%03d' % t}"
+    FileUtils.mkdir_p this_test_root
+
+    kbest_ops   = [
+      "--out #{this_test_root}",
+      "--k 3",
+      "--diff 0.05",
+      "--min 3",
+      "--max 30"
+    ].join(' ')
+
+    cmd = [
+      "#{HOME}/projects/msc-thesis/bin/ppm-rr",
+      "--config #{TEST_ROOT}/common.cfg",
+      "--max_threads #{t}",
+      "--output_dir #{this_test_root}",
+      "--scene_dir scenes/#{scene}",
+      "--scene_file #{scene}.scn"
+    ].join(' ')
+
+    puts   "#{KBEST} #{kbest_ops} \"#{cmd}\""
+    system "#{KBEST} #{kbest_ops} \"#{cmd}\""
+  end
+end
---
+++ tests/ppm-rr/gpu/qsub-511.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#PBS -V
+#PBS -N ppm-rr-scal-511
+#PBS -l nodes=1:r511:ppn=24
+#PBS -l walltime=20:00:00
+#PBS -m bea
+#PBS -e ppm-rr-scal-511.err
+#PBS -o ppm-rr-scal-511.out
+
+
+source /etc/profile.d/env-modules.sh
+module load gcc/4.6.3
+module load boost/1.49.0
+module load cuda-5.0
+module load freeimage
+
+cd $HOME/projects/msc-thesis
+echo $PWD
+echo $PATH
+ls /share/edu-mei
+tests/ppm-rr/gpu/job.rb 
