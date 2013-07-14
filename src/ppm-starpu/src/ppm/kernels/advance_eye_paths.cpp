3bfe08267ba811132b329d51642b59e059c891fb
Miguel Palhas <mpalhas@gmail.com>
2013-07-14 23:02:30 +0100

    moves starpu_handles to a more global scope

--- src/ppm-starpu/src/ppm/kernels/advance_eye_paths.cpp
+++ src/ppm-starpu/src/ppm/kernels/advance_eye_paths.cpp
@@ -6,37 +6,21 @@
 namespace ppm { namespace kernels {
 
 void advance_eye_paths(
-    vector<HitPointStaticInfo>& hit_points,
-    RayBuffer&                  ray_hit_buffer,
-    vector<EyePath>&            eye_paths,
-    vector<unsigned>&           eye_paths_indexes,
-    vector<Seed>&               seed_buffer) {
-
-  // hit points static info
-  starpu_data_handle_t handle_hit_points;
-  starpu_vector_data_register(&handle_hit_points, 0, (uintptr_t)&hit_points[0], hit_points.size(), sizeof(HitPointStaticInfo));
-  // hit buffer
-  starpu_data_handle_t handle_hits;
-  starpu_vector_data_register(&handle_hits, 0, (uintptr_t)ray_hit_buffer.GetHitBuffer(), ray_hit_buffer.GetRayCount(), sizeof(RayHit));
-  // eye paths
-  starpu_data_handle_t handle_eye_paths;
-  starpu_vector_data_register(&handle_eye_paths, 0, (uintptr_t)&eye_paths[0], eye_paths.size(), sizeof(EyePath));
-  // eye paths indexes
-  starpu_data_handle_t handle_indexes;
-  starpu_vector_data_register(&handle_indexes, 0, (uintptr_t)&eye_paths_indexes[0], eye_paths_indexes.size(), sizeof(unsigned));
-  // seed buffer
-  starpu_data_handle_t handle_seeds;
-  starpu_vector_data_register(&handle_seeds, 0, (uintptr_t)&seed_buffer[0], seed_buffer.size(), sizeof(Seed));
+    starpu_data_handle_t hit_points_info,
+    starpu_data_handle_t hit_buffer,
+    starpu_data_handle_t eye_paths,
+    starpu_data_handle_t eye_paths_indexes,
+    starpu_data_handle_t seed_buffer) {
 
   // task definition
   struct starpu_task* task = starpu_task_create();
   task->synchronous = 1;
   task->cl = &codelets::advance_eye_paths;
-  task->handles[0] = handle_hit_points;
-  task->handles[1] = handle_hits;
-  task->handles[2] = handle_eye_paths;
-  task->handles[3] = handle_indexes;
-  task->handles[4] = handle_seeds;
+  task->handles[0] = hit_points_info;
+  task->handles[1] = hit_buffer;
+  task->handles[2] = eye_paths;
+  task->handles[3] = eye_paths_indexes;
+  task->handles[4] = seed_buffer;
   task->cl_arg      = &codelets::generic_args;
   task->cl_arg_size = sizeof(codelets::generic_args);
 
--- src/ppm-starpu/src/ppm/kernels/generate_eye_paths.cpp
+++ src/ppm-starpu/src/ppm/kernels/generate_eye_paths.cpp
@@ -6,23 +6,17 @@
 namespace ppm { namespace kernels {
 
 void generate_eye_paths(
-    vector<EyePath>& eye_paths,
-    vector<Seed>&    seed_buffer) {
+    starpu_data_handle_t eye_paths,
+    starpu_data_handle_t seed_buffer) {
 
   // handles
-  // eye_paths
-  starpu_data_handle_t handle_eye_paths;
-  starpu_vector_data_register(&handle_eye_paths, 0, (uintptr_t)&eye_paths[0], eye_paths.size(), sizeof(EyePath));
-  // seed_buffer
-  starpu_data_handle_t handle_seed_buffer;
-  starpu_vector_data_register(&handle_seed_buffer, 0, (uintptr_t)&seed_buffer[0], seed_buffer.size(), sizeof(Seed));
 
   // task definition
   struct starpu_task* task = starpu_task_create();
   task->synchronous = 1;
   task->cl = &codelets::generate_eye_paths;
-  task->handles[0] = handle_eye_paths;
-  task->handles[1] = handle_seed_buffer;
+  task->handles[0] = eye_paths;
+  task->handles[1] = seed_buffer;
   task->cl_arg      = &codelets::generic_args;
   task->cl_arg_size = sizeof(codelets::generic_args);
 
