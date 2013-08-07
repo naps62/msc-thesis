26451588066188e7e907550906a365e938786a61
Miguel Palhas <mpalhas@gmail.com>
2013-08-07 20:25:47 +0100

    udpates to cuda engine

--- src/sppmpa-starpu/src/ppm/cuda_engine.cu
+++ src/sppmpa-starpu/src/ppm/cuda_engine.cu
@@ -105,15 +105,36 @@ void CUDAEngine :: splat_to_film() {
   //kernels::cpu::splat_to_film_impl(sample_buffer, film, config.width, config.height);
 }
 
+template<class T>
+void cuda_alloc(T** ptr, unsigned num_elems) {
+  cudaMalloc(ptr, sizeof(T) * num_elems);
+}
+
 void CUDAEngine :: before() {
+  cudaFree(seeds);
+  cudaFree(eye_paths);
+  cudaFree(hit_points_info);
+  cudaFree(hit_points);
+  cudaFree(hash_grid);
+  cudaFree(hash_grid_lengths);
+  cudaFree(hash_grid_indexes);
+  cudaFree(live_photon_paths);
 }
 
 void CUDAEngine :: after() {
 
+  cuda_alloc(&seeds,            config.seed_size);
+  cuda_alloc(&eye_paths,        config.total_hit_points);
+  cuda_alloc(&hit_points_info,  config.total_hit_points);
+  cuda_alloc(&hit_points,       config.total_hit_points);
+  cuda_alloc(&hash_grid,         8 * config.total_hit_points);
+  cuda_alloc(&hash_grid_lengths, config.total_hit_points);
+  cuda_alloc(&hash_grid_indexes, config.total_hit_points);
+  cuda_alloc(&live_photon_paths, config.photons_per_iter);
 }
 
-void CUDAEngine :: wait_for_all() {
 
+void CUDAEngine :: wait_for_all() {
 }
 
 }
