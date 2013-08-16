64840fbd10e7dfa6f1478efbe0948dd5c7a7bce0
Miguel Palhas <mpalhas@gmail.com>
2013-08-16 02:21:52 +0100

    added max_iters to rr scal tests

--- tests/final/scalability_all_rr.rb
+++ tests/final/scalability_all_rr.rb
@@ -4,7 +4,8 @@ require './lib/common'
 
 args = {scene: ['kitchen', 'cornell', 'luxball'],
         max_threads: [1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 64, 128, 256],
-        photons_per_iter: [20,21,22]
+        photons_per_iter: [20,21,22],
+        max_iters: 100
 }
 
 KEPLER_ENV = 'CUDA_VISIBLE_DEVICES="0"'
